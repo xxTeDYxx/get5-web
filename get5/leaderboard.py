031b635a152f5bd48d0deeb8158ec37ca192cc55
PhlexPlexico <PhlexPlexico@users.noreply.github.com>
2019-03-30 23:45:10 -0600

    Development (#36)
    * Update match.html
    
    Sorting on kills?
    
    * Update match.html
    
    Added sanity check as some servers are also likely to not have GoTV enabled on their server. Need to adjust match config as well to check if the port number is empty, or if tv_enabled is 0.
    
    * Update match.html
    
    * Update match.py
    
    * Update match.html
    
    Please Travis just spare me some mercy
    
    * Update match.html
    
    Have mercy 2.0 aka i hate myself.
    
    * Update match.html
    
    Attempting to sort again.
    
    * Update match.html
    
    Yay. Adjusting None values again.
    
    * Update match.py
    
    Adjusting gotv string again.
    
    * Update README.md
    
    * Adjusted sort order.
    
    * Using temp workaround
    
    For team logos. Now using TeamLogoManager plugin, due to an existing issue with Get5. However, in order to connect to the server you must have sv_allowupload 1 or you will not be able to connect.
    
    * Super basic leaderboard based off of wins only.
    
    Going to update with losses, and round difference as well.
    
    * Updated to include round diff.
    
    Math is wrong, and this is really inefficient.
    
    * Adjust the team standings round diff.
    
    Messed up and didn't keep adding the current score. This should fix it.
    
    * Update leaderboard.py
    
    * Changes to leaderboard and matches.
    
    Leaderboard will now link to the teams. Moved TeamLeaderboard class from leaderboard to models, left todo to create a better class for the leaderboard instead of using dictionaries. Updated util to use command to receive values from RCON. Updated match to enforce get5 auth, so that when a match is scheduled through the web panel it's forced to be authenticated.
    
    * Leaderboard errors.
    
    Adjusting team names and trying to get login to show correct menu.
    
    * Formatting and adding leaderboard into header.
    
    * Leaderboar menu fix.
    
    * Update match.py
    
    Messed up the rcon move last night. Adjusting params to intake server, and then adding in checks to util for no response.
    
    * Update util.py
    
    Adding in some checks so we don't just run into an exception.
    
    * Update match.py
    
    Added in some more logic check to matches.
    
    * Update util.py
    
    * Update models.py
    
    * Update match.py
    
    * Update match.py
    
    server_available.
    
    * Update models.py
    
    Receive, not whatever abomination I put.
    
    * Update models.py
    
    * Update match.py
    
    * Updated to add active class for leaderboard.
    
    Removing message about team logos.
    
    * Fixed leaderboard issue when matches are in progress.

--- get5/leaderboard.py
+++ get5/leaderboard.py
@@ -5,6 +5,7 @@ import get5
 from get5 import app, db, BadRequestError, config_setting
 from models import User, Team, Match, GameServer, MapStats, TeamLeaderboard
 from collections import OrderedDict, defaultdict
+from datetime import datetime
 import util
 import re
 from copy import deepcopy
@@ -15,8 +16,8 @@ leaderboard_blueprint = Blueprint('leaderboard', __name__)
 
 @leaderboard_blueprint.route('/leaderboard')
 def leaderboard():
-    totalMatches = Match.query.order_by(-Match.id).filter_by(
-        cancelled=False)
+    totalMatches = Match.query.order_by(-Match.id).filter(
+        Match.cancelled == False, Match.end_time.isnot(None))
     allTeams = Team.query.order_by(-Team.id)
     # Shoutouts to n3rds.
     dTeamStandings = defaultdict(lambda: {'teamid': 0, 'wins': 0, 'losses': 0, 'rounddiff': 0})
