4f8cb39f70169607a9677fb79d0aa846d2ea3e96
PhlexPlexico <3514085+PhlexPlexico@users.noreply.github.com>
2019-04-30 10:14:57 -0600

    Logo Directory Updates
    Not using a full path, I hope this doesn't break anything.

--- get5/__init__.py
+++ get5/__init__.py
@@ -42,7 +42,7 @@ sys.setdefaultencoding('utf-8')
 # Import the Flask Framework
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('prod_config.py')
-LOGO_FOLDER = '/var/www/get5-web/get5/static/img/logos/'
+LOGO_FOLDER = './static/img/logos/'
 app.config['LOGO_FOLDER'] = LOGO_FOLDER
 
 # Setup caching
