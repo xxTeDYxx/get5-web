d2a473f41004c71e1a92eaaf9bfe1444ed7653f1
Evan <etc114@mail.usask.ca>
2019-05-04 17:43:29 -0600

    Include crypto resources in utils.

--- get5/util.py
+++ get5/util.py
@@ -1,7 +1,10 @@
 import os
 import socket
 import subprocess
-
+import base64
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from Crypto import Random
 
 def as_int(val, on_fail=0):
     if val is None:
@@ -121,3 +124,24 @@ def get_version():
         return subprocess.check_output(cmd, cwd=root_dir).strip()
     except (OSError, subprocess.CalledProcessError):
         return None
+
+def encrypt(key, source, encode=True):
+    key = SHA256.new(key).digest()  # use SHA-256 over our key to get a proper-sized AES key
+    IV = Random.new().read(AES.block_size)  # generate IV
+    encryptor = AES.new(key, AES.MODE_CBC, IV)
+    padding = AES.block_size - len(source) % AES.block_size  # calculate needed padding
+    source += chr(padding) * padding # Python 2.x: source += chr(padding) * padding # Python 3.x bytes([padding]) * padding
+    data = IV + encryptor.encrypt(source)  # store the IV at the beginning and encrypt
+    return base64.b64encode(data).decode("latin-1") if encode else data
+
+def decrypt(key, source, decode=True):
+    if decode:
+        source = base64.b64decode(source.encode("latin-1"))
+    key = SHA256.new(key).digest()  # use SHA-256 over our key to get a proper-sized AES key
+    IV = source[:AES.block_size]  # extract the IV from the beginning
+    decryptor = AES.new(key, AES.MODE_CBC, IV)
+    data = decryptor.decrypt(source[AES.block_size:])  # decrypt
+    padding = ord(data[-1])  # pick the padding value from the end; Python 2.x: ord(data[-1]) # Python 3.x: data[-1]
+    if data[-padding:] != chr(padding) * padding:  # Python 2.x: chr(padding) * padding # Python 3.x: bytes([padding]) * padding:
+        raise None
+    return data[:-padding]  # remove the padding
