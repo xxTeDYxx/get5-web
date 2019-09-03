a992725e6cface77c15c909d6120da111bba2f55
Evan <etc114@mail.usask.ca>
2019-09-02 19:24:12 -0600

    Resolves bug #134.

--- get5/models.py
+++ get5/models.py
@@ -699,20 +699,22 @@ class PlayerStats(db.Model):
         return get_steam_name(self.steam_id)
 
     def get_rating(self):
-        AverageKPR = 0.679
-        AverageSPR = 0.317
-        AverageRMK = 1.277
-        KillRating = float(self.kills) / float(self.roundsplayed) / AverageKPR
-        SurvivalRating = float(self.roundsplayed -
-                               self.deaths) / self.roundsplayed / AverageSPR
-        killcount = float(self.k1 + 4 * self.k2 + 9 *
-                          self.k3 + 16 * self.k4 + 25 * self.k5)
-        RoundsWithMultipleKillsRating = killcount / \
-            self.roundsplayed / AverageRMK
-        rating = (KillRating + 0.7 * SurvivalRating +
-                  RoundsWithMultipleKillsRating) / 2.7
-        return rating
-
+        try:
+            AverageKPR = 0.679
+            AverageSPR = 0.317
+            AverageRMK = 1.277
+            KillRating = float(self.kills) / float(self.roundsplayed) / AverageKPR
+            SurvivalRating = float(self.roundsplayed -
+                                self.deaths) / self.roundsplayed / AverageSPR
+            killcount = float(self.k1 + 4 * self.k2 + 9 *
+                            self.k3 + 16 * self.k4 + 25 * self.k5)
+            RoundsWithMultipleKillsRating = killcount / \
+                self.roundsplayed / AverageRMK
+            rating = (KillRating + 0.7 * SurvivalRating +
+                    RoundsWithMultipleKillsRating) / 2.7
+            return rating
+        except ZeroDivisionError:
+            return 0
     def get_kdr(self):
         if self.deaths == 0:
             return float(self.kills)
